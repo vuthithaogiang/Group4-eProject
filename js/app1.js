624974ca17f45833cf6f4061a9c92b7fff8743bb
vuthithaogiang <vuthithaogiang050400@gmail.com>
2023-02-05 16:35:45 +0700

    Update app1.js

--- js/app1.js
+++ js/app1.js
@@ -14,14 +14,14 @@ angular.module("app", ["ngRoute"]).controller("MainController", function () {
             fullname: 'Nguyen Huy Hoang',
             username: 'hoang',
             email: 'hoang@gmail.com',
-            phone: '111 111 111', 
+            phone: '111 111 111',
             id: 0
         },
         {
             fullname: 'Vu Thi Thao Giang',
-            username: 'thao giang', 
+            username: 'thao giang',
             email: 'thaogiang@gmail.com',
-            phone: '111 222 333', 
+            phone: '111 222 333',
             id: 1
         },
         {
@@ -31,25 +31,25 @@ angular.module("app", ["ngRoute"]).controller("MainController", function () {
             email: 'thanhnguyen@gmail.com',
             phone: '000 444 555',
             id: 2
-        }, 
+        },
         {
-            fullname: 'Nguyen Van Hoa', 
+            fullname: 'Nguyen Van Hoa',
             username: 'hoa nguyen',
             email: 'hoanguyen@gmail.com',
-            phone: '999 999 999', 
+            phone: '999 999 999',
             id: 3
-        }, 
+        },
         {
             fullname: 'Vu Quynh Anh',
             username: 'anh vu',
             email: 'vuanh@gmail.com',
-            phone: '555 555 555', 
+            phone: '555 555 555',
             id: 4
         },
 
     ];
 
-    vm.orders =[
+    vm.orders = [
         {
             id: 1,
             title: 'Username Ascending',
@@ -62,7 +62,7 @@ angular.module("app", ["ngRoute"]).controller("MainController", function () {
             title: 'Username Descending',
             key: 'username',
             reverse: true
-        }, 
+        },
         {
             id: 3,
             title: 'Email Ascending',
@@ -72,59 +72,70 @@ angular.module("app", ["ngRoute"]).controller("MainController", function () {
         },
         {
             is: 4,
-            title: 'Email Descending', 
+            title: 'Email Descending',
             key: 'email',
             reverse: true
         }
     ];
 
+
     vm.order = vm.orders[0];
     vm.new = {};
     vm.addShow = function () {
 
-        if(vm.new.id == null) {
-            vm.new.id = uid++;
-            vm.shows.push(vm.new);
-        }
-        else{
-            for( i in vm.shows){
-                if(vm.shows[i].id == vm.new.id){
-                    vm.shows[i] = vm.new;
+        if (vm.addForm.$valid) {
+            console.log('form is valid');
+    
+            if (vm.new.id == null) {
+                vm.new.id = uid++;
+                vm.shows.push(vm.new);
+            }
+            else {
+                for (i in vm.shows) {
+                    if (vm.shows[i].id == vm.new.id) {
+                        vm.shows[i] = vm.new;
+                    }
                 }
+
             }
 
         }
+        else {
+            console.log('form is invalid!');
+        }
 
         vm.new = {};
-        
-       
+
     };
+
 
     console.log(vm);
 
-    vm.delete = function (id){
+    vm.delete = function (id) {
 
-        for( i in vm.shows){
-            if(vm.shows[i].id == id){
+        for (i in vm.shows) {
+            if (vm.shows[i].id == id) {
                 vm.shows.splice(i, 1);
                 vm.new = {};
             }
         }
-   
+        vm.new = {};
+
+
 
     };
 
-    vm.edit = function (id){
+    vm.edit = function (id) {
         console.log('clicked');
         console.log(id);
 
-        for( i in vm.shows){
-            if(vm.shows[i].id == id){
+        for (i in vm.shows) {
+            if (vm.shows[i].id == id) {
                 vm.new = angular.copy(vm.shows[i]);
             }
         }
-    
-        
+
+
     };
 
 
